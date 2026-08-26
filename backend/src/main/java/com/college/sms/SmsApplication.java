a2d96b067861f2969416482968dd69056ab8686d
gurukowshikpoola <gurukowshik1438@gmail.com>
2026-08-26 10:47:09 +0530

    Add Dockerfile for deployment today

--- backend/src/main/java/com/college/sms/SmsApplication.java
+++ backend/src/main/java/com/college/sms/SmsApplication.java
@@ -2,12 +2,19 @@ package com.college.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Entry point for the Student Management System Spring Boot application.
  */
 @SpringBootApplication
-public class SmsApplication {
+public class SmsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SmsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
